9e8eb0eab36410e97d9f4213f8c98552b392abc7
Shashwat Behera <124123097+slogomaan@users.noreply.github.com>
2024-06-09 21:10:51 +0530

    Update settings.py

--- sptxtl/settings.py
+++ sptxtl/settings.py
@@ -49,21 +49,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("POSTGRES_DATABASE"),
-        "USER": env('POSTGRES_USER'),
-        "PASSWORD": env('POSTGRES_PASSWORD'),
-        "HOST": env("POSTGRES_HOST"),
-        "URL": env("POSTGRES_URL"),
-        "PORT": "5432"
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 WSGI_APPLICATION = 'sptxtl.wsgi.app'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LANGUAGE_CODE = 'en-us'
