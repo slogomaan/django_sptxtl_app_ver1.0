92b144cfd0369ca3cdde047c608621674d0785b4
slogomaan <shashwatbehera@icloud.com>
2024-06-09 19:07:56 +0530

    added vercel.json file

--- sptxtl/wsgi.py
+++ sptxtl/wsgi.py
@@ -1,14 +1,7 @@
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sptxtl.settings')
 
 application = get_wsgi_application()
-
-<<<<<<< HEAD
-app = application
-=======
-app = application
->>>>>>> bf28f181bbe64b7d315638678f6616aa969b1ecc
